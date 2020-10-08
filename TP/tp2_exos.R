32530f4ebe8d1370673ab4e39f14f721ea2c6730
mordor-ai <github@pellegrin.me>
2020-10-08 11:51:19 +0200

    added tp3

--- TP/tp2_exos.R
+++ TP/tp2_exos.R
@@ -48,7 +48,58 @@ print(paste('excrat variance biais /sans biais',(var(X$samples)-my_variance(X$sa
 
 #question 3 : Via notre échantillon, estimer la probabilité de |X|>2.
 
-X <- data.frame(samples=rnorm(10, 0, 1))
+X <- data.frame(samples=rnorm(10000, 0, 1))
+head(X)
+
+print(paste('La probabilité empirique est', sum(abs(X)>2)/N))
+#sigma =  1
+# 95 % des echanitillo,ns sont compris entre -sigma et sigma
+# ,CORRESPOND À  LA MARGE D'ERREUR
+
+
+# Question 4  : 
+
+
+
+X <- data.frame(samples=rnorm(10000, 0, 1))
+head(X)
+
+print(paste('La probabilité empirique est', sum(abs(X)>3)/N))
+
+
+
+
+# importance sampling
+
+# simulation selon la lio normale
+N=  10000
+Mygamma <- data.frame(samples=rnorm(N, 0, 1))
+head(Mygamma)
+Mygamma$density = dnorm(Mygamma$samples)
+head(Mygamma)
+
+
+
+density_g <- function(x){
+  (dnorm(x, mean = -3, sd = 1) + dnorm(x, mean = 0, sd = sqrt(10)) + dnorm(x, mean = 1, sd = 1))/3
+}
+
+Mygamma$g = density_g(Mygamma$samples)
+head(Mygamma)
+Mygamma$g_gamma =  Mygamma$samples*Mygamma$g/Mygamma$density
+head(Mygamma)
+sum(Mygamma$g_gamma)/count(Mygamma)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
